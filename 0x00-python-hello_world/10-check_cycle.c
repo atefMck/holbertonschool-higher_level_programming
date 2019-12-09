e1ed23fd2fd1b19b738241cc740189593e6e4aeb
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-09 18:30:19 +0100

    Task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* check_cycle - check for cycle
+* @list: the list
+* Return: has cycle or not
+*/
+int check_cycle(listint_t *list)
+{
+listint_t *fastp;
+listint_t *slowp;
+fastp = list;
+slowp = list;
+while (fastp->next->next)
+{
+slowp = slowp->next;
+fastp = fastp->next->next;
+if (slowp->n == fastp->n)
+return (1);
+}
+return (0);
+}
