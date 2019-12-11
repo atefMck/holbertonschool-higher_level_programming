2fcdf55be63dadcf29ce0fcd396b82c4163af3aa
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-11 11:08:55 +0100

    Task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        ld = (-number) % 10
+    elif number == 0:
+        ld = 0
+    else:
+        ld = number % 10
+    print("{:d}".format(ld), end='')
+    return(ld)
