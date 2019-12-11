b02d5ea7b2f59681c540aa84f4763f38ffebe500
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-11 11:36:10 +0100

    Task 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* insert_node - insert new node in list
+* @head: head of list
+* @number: the number to add
+* Return: pointer to node
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+listint_t *newNode;
+listint_t *curr;
+if (!head)
+return (NULL);
+if (!*head)
+{
+newNode = add_nodeint_end(head, number);
+return (newNode);
+}
+curr = *head;
+newNode = malloc(sizeof(listint_t));
+if (!newNode)
+return (NULL);
+newNode->n = number;
+while (curr)
+{
+if (newNode->n > curr->next->n)
+curr = curr->next;
+else
+{
+newNode->next = curr->next->next;
+curr->next = newNode;
+break;
+}
+}
+return (newNode);
+}
