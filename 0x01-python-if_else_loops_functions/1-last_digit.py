aa599d9e71a987c4caea68f95b0c2e52b41ce7a0
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-11 10:13:04 +0100

    Task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,10 @@ elif number == 0:
     ld = 0
 else:
     ld = number % 10
-print("Last digit of", number, "is", ld, end = " ")
+print("Last digit of", number, "is", ld, end='')
 if ld > 5:
     print("and is greater than 5")
-elif ld < 6:
+elif ld < 6 and ld != 0:
     print("and is less than 6 and not 0")
 elif ld == 0:
     print("and is 0")
