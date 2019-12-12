7aef3aefe9290cf1bffd1c1b231cef2db74e16f0
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-12 20:38:38 +0100

    Task 2 fixing output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,9 @@ if __name__ == "__main__":
     import sys
 argv = sys.argv
 len = len(argv)
-print("{:d} arguments.".format(len - 1))
 if len > 1:
+    print("{:d} arguments:".format(len - 1))
     for i in range(1, len):
         print("{:d}: {:s}".format(i, argv[i]))
+else:
+    print("{:d} arguments.".format(len - 1))
