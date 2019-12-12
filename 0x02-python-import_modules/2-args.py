119c6984012fea6aec1a460e860fde09dbb310bb
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-12 20:41:35 +0100

    Task 2 fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,12 @@ if __name__ == "__main__":
 argv = sys.argv
 len = len(argv)
 if len > 1:
-    print("{:d} arguments:".format(len - 1))
-    for i in range(1, len):
-        print("{:d}: {:s}".format(i, argv[i]))
+    if len == 2:
+        print("{:d} argument:".format(len - 1))
+        print("{:d}: {:s}".format(1, argv[1]))
+    else:
+        print("{:d} arguments:".format(len - 1))
+        for i in range(1, len):
+            print("{:d}: {:s}".format(i, argv[i]))
 else:
     print("{:d} arguments.".format(len - 1))
