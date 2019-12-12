6b65a152f55ca71f25057a400d2da2e9e8e46392
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-12 21:23:35 +0100

    Taks 102 Fixed

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from magic_calculation_102 import add, sub
-if a < b:
-    c = add(a, b)
-    for i in range(4, 6):
-        c = add(c, i)
-    return(c)
-else:
-    return(sub(a, b))
+def magic_calculation(a, b):
+    if __name__ == '__main__':
+        from magic_calculation_102 import add
+        from magic_calculation_102 import sub
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return(c)
+    else:
+        return(sub(a, b))
