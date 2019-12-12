c91c02fa65b1e2c006dbf06c9b33effee0db9a3c
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-12 20:36:55 +0100

    Task 2 fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 argv = sys.argv
 len = len(argv)
-print("{:d} arguments.".format(len))
+print("{:d} arguments.".format(len - 1))
 if len > 1:
     for i in range(1, len):
         print("{:d}: {:s}".format(i, argv[i]))
