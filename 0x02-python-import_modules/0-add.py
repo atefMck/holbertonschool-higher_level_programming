7efbec03a2699928902836531ec3a9ef4fa4cb86
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-12 20:23:15 +0100

    Task 0 fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add
 a = 1
 b = 2
-add = __import__("add_0")
-print("{:d} + {:d} = {:d}".format(a, b, add.add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
