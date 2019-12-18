c01d60b7419541820c72eeed6d85dc8fe9d601f5
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-18 17:53:39 +0100

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    c = 0
     if len(my_list) > 0:
+        c = my_list[0]
         for element in my_list:
             if c < element:
                 c = element
