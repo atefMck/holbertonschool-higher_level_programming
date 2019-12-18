4211b31eccecaac4e0d2535070c4df9ad4d0b925
Atef Mechken <atef.mechken@youngminds.tn>
2019-12-18 17:14:49 +0100

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
+    for i in range(len(matrix)):
         space = ""
-        for j in range(0, len(matrix[i])):
+        for j in range(len(matrix[i])):
             print(b, end="")
             print("{:d} ".format(matrix[i][j]), end="")
             space = " "
