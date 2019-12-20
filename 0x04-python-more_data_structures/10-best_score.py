b0d85d8b5ab361043e6e0ba268e562fd0418a93f
boumaiza-kais <1110@holbertonschool.com>
2019-12-20 11:08:03 +0100

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is not None:
+        c = -90000
+        for keys in a_dictionary.keys():
+            if a_dictionary[keys] > c :
+                c = a_dictionary[keys]
+                r = keys
+        return(r)
+    return(None)
