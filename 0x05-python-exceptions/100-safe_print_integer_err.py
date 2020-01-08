fedd8f45c58c2e41b345f7110234815b4fc9b365
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-08 18:02:38 +0100

    Fixed return to line in output

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError) as error:
-        sys.exit("Exception: {}".format(error.args[0]))
+        sys.stderr.write("Exception: {}\n".format(error.args[0]))
         return (False)
