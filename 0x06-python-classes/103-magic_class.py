1acd0145d89b8b1c81e63393b176d071b66e4922
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-09 22:51:53 +0100

    Fixede init method name task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,9 +3,8 @@ import math
 
 
 class MagicClass:
-    ''' Magic Class '''
 
-    def __init(self, radius=0):
+    def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
                 raise TypeError("radius must be a number")
