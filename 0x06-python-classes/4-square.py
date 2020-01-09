bdd12608d7d468f263813be158b0d6c835faf04e
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-09 20:13:20 +0100

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,12 +12,14 @@ class Square:
     def area(self):
         return (self.__size ** 2)
 
+    @property
     def size(self):
         return (self.__size)
 
+    @size.setter
     def size(self, value):
         if isinstance(value, int):
-            if size < 0:
+            if value < 0:
                 raise ValueError("size must be >= 0")
             else:
                 self.__size = value
