55aed1c75a197994f345c065cb23f7e586136a6f
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-28 22:30:24 +0100

    Fixed minor issues in setters of Rectangle class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """ tests for Base class """
+    def test_base(self):
+        """
+        Tests when d is given or not
+        """
+        b = Base()
+        self.assertEqual(b.id, 1)
