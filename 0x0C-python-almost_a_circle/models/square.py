a4fff98edad16fc9e910352c32f889397cd6ddb8
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-28 20:51:07 +0100

    Fixed Square class str method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,10 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init method for Square class"""
         Rectangle.__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """method formatting string Square output"""
+        string0 = "[{}] ({}) ".format(self.__class__.__name__, self.id)
+        string1 = "{}/{} ".format(self.x, self.y)
+        string2 = "- {}".format(self.width)
+        return(string0 + string1 + string2)
