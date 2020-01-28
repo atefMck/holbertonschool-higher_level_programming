24252d89aaf5cc7da592f89957c8182d69aa0f43
Atef Mechken <atef.mechken@youngminds.tn>
2020-01-28 20:53:58 +0100

    Replaced init method with super

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """init method for Square class"""
-        Rectangle.__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """method formatting string Square output"""
