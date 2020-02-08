a1908fd05b7ef6ae3fdc025131bd4d1a7f79b8fa
Atef Mechken <atef.mechken@youngminds.tn>
2020-02-08 13:11:41 +0100

    Removing main func from func file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,7 +3,3 @@ def number_of_lines(filename=""):
     with open(filename, 'r', encoding="utf-8") as file:
         count = len(file.readlines())
     return count
-
-filename = "my_file_0.txt"
-nb_lines = number_of_lines(filename)
-print("{} has {:d} lines".format(filename, nb_lines))
