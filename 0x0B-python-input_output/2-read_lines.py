b2af74a4995cd237a561d63544cdec518f203bc2
Atef Mechken <atef.mechken@youngminds.tn>
2020-02-08 13:17:03 +0100

    Task 1

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    with open(filename, 'r', encoding="utf-8") as file:
+        if nb_lines <= 0:
+            print(file.read(), end="")
+        else:
+            for i in range(nb_lines):
+                print(file.readline(), end="")
