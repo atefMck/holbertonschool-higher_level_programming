49d0e077aa182e7894fef54c7e05dbb6bcf44a66
atefsama666 <mechken.atef@gmail.com>
2020-03-29 22:16:23 +0100

    Pep8 styled all task 100 files

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -13,4 +13,5 @@ class State(Base):
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="save-update, delete")
+    cities = relationship("City", backref="state",
+                          cascade="save-update, delete")
