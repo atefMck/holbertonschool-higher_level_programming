07100ff86173ddac7177d237979fd3ff8ea6b163
atefsama666 <mechken.atef@gmail.com>
2020-03-29 22:14:52 +0100

    Task 100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' Module to print all states '''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    ''' Boombastic '''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="save-update, delete")
