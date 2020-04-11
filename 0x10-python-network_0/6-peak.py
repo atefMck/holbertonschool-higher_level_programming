9b2c7c515a7841481bebf35335fcbe2555112ef5
atefsama666 <mechken.atef@gmail.com>
2020-04-11 01:39:20 +0100

    Pep8 solution file

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """ find_peak function """
+
+
 def find_peak(list_of_integers):
-	start = 0
-	length = len(list_of_integers)
-	end = length - 1
-	peak = None
-	while start <= end:
-		mid = int((start + end) / 2)
-		peak = list_of_integers[mid]
-		left = list_of_integers[mid - 1]
-		right = list_of_integers[mid + 1]
-		if (mid == 0 or peak >= left) and (mid == length - 1 or peak >= right):
-			return peak
-		elif mid > 0 and peak < left:
-			end = mid - 1
-		else:
-			start = mid + 1
-	return None
+    start = 0
+    length = len(list_of_integers)
+    end = length - 1
+    peak = None
+    while start <= end:
+        mid = int((start + end) / 2)
+        peak = list_of_integers[mid]
+        left = list_of_integers[mid - 1]
+        right = list_of_integers[mid + 1]
+        if (mid == 0 or peak >= left) and (mid == length - 1 or peak >= right):
+            return peak
+        elif mid > 0 and peak < left:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
