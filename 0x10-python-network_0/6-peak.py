a1123ecdf2e1a12b347cff50b9a7f5496a4be5e9
atefsama666 <mechken.atef@gmail.com>
2020-04-11 01:45:40 +0100

    Task 6 fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,14 +8,14 @@ def find_peak(list_of_integers):
     end = length - 1
     peak = None
     while start <= end:
-        mid = int((start + end) / 2)
+        mid = (start + end) // 2
         peak = list_of_integers[mid]
         left = list_of_integers[mid - 1]
         right = list_of_integers[mid + 1]
         if (mid == 0 or peak >= left) and (mid == length - 1 or peak >= right):
-            return peak
+            break
         elif mid > 0 and peak < left:
             end = mid - 1
         else:
             start = mid + 1
-    return None
+    return peak
