fecdc9ed210f9f6eb0f2661ca93e233239c4ac09
atefsama666 <mechken.atef@gmail.com>
2020-05-28 14:26:24 +0100

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Python script that fetches https://intranet.hbtn.io/status """
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
+	req = f.read()
+	print('Body response:')
+	print('\t- type:', type(req))
+	print('\t- content:', req)
+	print('\t- utf8 content:', req.decode('utf-8'))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+Today we do more python networking
