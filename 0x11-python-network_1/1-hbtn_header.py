6ccceafd2db27de11426f562c5696b68e2a06a2c
atefsama666 <mechken.atef@gmail.com>
2020-05-28 14:46:09 +0100

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-"""
-A script that takes in a URL, sends a request to the URL and displays
-the value of the X-Request-Id variablefound in the header of the response.
-"""
+""" Python script that fetches https://intranet.hbtn.io/status """
 from sys import argv
 from urllib import request
 
 
 def run():
-    """
-    Sends request to URL and displays X-REquest-Id values.
-    """
-    url = argv[1]
-    with request.urlopen(url) as res:
+    """ Intermediate method for checker. """
+    with request.urlopen(argv[1]) as res:
         print(res.headers['X-Request-Id'])
 
 
