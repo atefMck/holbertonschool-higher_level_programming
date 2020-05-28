8f74bd0fb85c8a8f22067a01bbf5e143ca53913e
atefsama666 <mechken.atef@gmail.com>
2020-05-28 14:42:02 +0100

    Task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Python script that fetches https://intranet.hbtn.io/status """
-import urllib.request
+from urllib import request
 from sys import argv
 
-with urllib.request.urlopen(argv[1]) as req:
+with request.urlopen(argv[1]) as req:
     print(req.headers['X-Request-Id'])
