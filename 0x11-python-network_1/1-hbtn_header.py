84849356bae075ddc282c5834133c2ee731242a8
atefsama666 <mechken.atef@gmail.com>
2020-05-29 10:14:15 +0100

    Task 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,11 +4,11 @@ from sys import argv
 from urllib import request
 
 
-def run():
+def execute():
     """ Intermediate method for checker. """
     with request.urlopen(argv[1]) as res:
         print(res.headers['X-Request-Id'])
 
 
 if __name__ == "__main__":
-    run()
+    execute()
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Python script """
+from sys import argv
+from urllib import request
+from urllib import parse
+
+
+def execute():
+    """ Intermediate method for checker. """
+    data = parse.urlencode({"email": argv[2]})
+    data = data.encode('ascii')
+    with request.urlopen(argv[1], data) as res:
+        req = res.read()
+        print(req.decode('utf-8'))
+
+
+if __name__ == "__main__":
+    execute()
