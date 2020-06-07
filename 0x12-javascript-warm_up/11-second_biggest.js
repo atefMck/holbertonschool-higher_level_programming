fb3580f9d289bfa3070dc9cb098a76ce98c1fcca
atefsama666 <mechken.atef@gmail.com>
2020-06-07 12:17:56 +0100

    Task 11 fixed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length < 4) {
   console.log(0);
 } else {
   const arr = process.argv.slice(2, process.argv.length);
-  arr.sort();
+  arr.sort(function(a, b){return a - b});
   console.log(arr[arr.length - 2]);
 }
